dd8f6587c4675b9a76824842a403dbf5355820f4
Robert Adamczewski <robert@adamczewski.me>
2021-04-07 20:12:19 +0200

    feat: add global styles to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
+import 'normalize.css';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  background: #f8f8f8;
+}
+`
+
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <header>
         <h1>NASA</h1>
       </header>
